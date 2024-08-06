1a74e4812cfddc86ad1c5aae0acbc5df920da4ed
Devskiller <owl@devskiller.com>
2024-08-06 11:33:58 +0000

    Candidate repo initialization

---
+++ src/selenium_extractor.py
@@ -0,0 +1,15 @@
+
+
+class SeleniumExtractor:
+    
+    def __init__(self, driver):
+        self.driver = driver
+
+    def click_send_button(self):
+        return
+
+    def get_information_from_hq(self):
+        return
+
+    def get_animals(self):
+        return
